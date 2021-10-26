95ff9a0181b3223a44ced37a8835847db394c320
Kruglov Andrei <dus.sneg@gmail.com>
2021-10-26 14:53:57 +0500

    Exercise 5.6.6 (2-nd Branch)

--- src/Case13.java
+++ src/Case13.java
@@ -1,6 +1,7 @@
 /*..*/
 public class Case13 {
      public static void main(String[] args) {
-            System.out.println("Text from the main program");
+         System.out.println("Text from the main program");
+         System.out.println("Text from the 2-nd branch");
         }
     }
